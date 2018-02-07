7f07cc4c8942b9395b2afaea32fd0063e66cf5f6
Jeias Soares <jeias.soares@gmail.com>
2018-02-06 23:32:54 -0200

    teste 2 husky

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 const a = 3;
 const b = 2;
-const d = 6;
 const c = 'fasjflsjfsjlfjslf';
 
 function sum() {
-  return a + b + c + d;
+  return a + b + c;
 }
 
-console.log('teste';
+console.log('teste');
