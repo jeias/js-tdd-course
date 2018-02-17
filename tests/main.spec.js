67fa7ccdf98b5e28392d7c9d940624c823f8d957
Jeias Soares <jeias.soares@gmail.com>
2018-02-17 19:49:26 -0200

    template

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,27 +1,9 @@
 import { assert } from 'chai';
-import FizzBuzz from '../src/main';
 
 describe ('Main', () => {
 
-  it('Should return `Fizz` when multiple of 3', () => {
-    assert.equal(FizzBuzz(3), 'Fizz');
-    assert.equal(FizzBuzz(6), 'Fizz');
+  it('Description', () => {
+    assert.equal(true, true);
   });
 
-  it('Should return `Buzz` when multiple of 5', () => {
-    assert.equal(FizzBuzz(5), 'Buzz');
-    assert.equal(FizzBuzz(10), 'Buzz');
-  });
-
-  it('Should return `FizzBuzz` when multiple of 5 and 3', () => {
-    assert.equal(FizzBuzz(15), 'FizzBuzz');
-  });
-
-  it('Should return the number when non-multiple', () => {
-    assert.equal(FizzBuzz(17), 17);
-  });
-
-  it('Should return 0 when 0', () => {
-    assert.equal(FizzBuzz(0), 0);
-  });
 });
